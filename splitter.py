caf09a04244f5ff745b2966cd7fb013f31ae63bc
Stefano Costantini <sc@stefano-mac.local>
2019-02-18 16:53:18 +0000

    some more code

--- splitter.py
+++ splitter.py
@@ -1,15 +1,20 @@
+import os
 from pydub import AudioSegment
 from pydub.utils import make_chunks
 
-myaudio = AudioSegment.from_file("myAudio.wav" , "wav")
 chunk_length_ms = 2000 # pydub calculates in millisec
-chunks = make_chunks(myaudio, chunk_length_ms) #Make chunks of two secs
+load_path = "/data/musicnet/musicnet/data"
+save_path = "/data/musicnet/musicnet/data_chunks"
+files = os.listdir(load_path)
 
-#Export all of the individual chunks as wav files
+for i, file in enumerate(files):
+    path_to_file = load_path + file
+    audio_file = AudioSegment.from_file(path_to_file , "wav")
+    chunks = make_chunks(audio_file, chunk_length_ms)
 
-for i, chunk in enumerate(chunks):
-    chunk_name = "chunk{0}.wav".format(i)
-    print "exporting", chunk_name
-    chunk.export(chunk_name, format="wav")
+    #Export all of the individual chunks as wav files
 
- 
+    for i, chunk in enumerate(chunks):
+        chunk_name = "chunk{0}.wav".format(i)
+        print "exporting", chunk_name
+        chunk.export(chunk_name, format="wav")
