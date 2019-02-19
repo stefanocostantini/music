4c89c3bb70ced0bb1a716ca03d1fc940b9a750b6
Stefano Costantini <sc@stefano-mac.local>
2019-02-19 11:26:47 +0000

    Completed first version of splitter code, need to test

--- splitter.py
+++ splitter.py
@@ -2,12 +2,13 @@ import os
 from pydub import AudioSegment
 from pydub.utils import make_chunks
 
-chunk_length_ms = 2000 # pydub calculates in millisec
-load_path = "/data/musicnet/musicnet/data"
-save_path = "/data/musicnet/musicnet/data_chunks"
+chunk_length_ms = 2000 # pydub calculates in millisecs
+load_path = "/data/music/musicnet/test/"
+save_path = "/data/music/musicnet/test_chunks/"
 files = os.listdir(load_path)
 
 for i, file in enumerate(files):
+    file_label = file.split(".")[0]
     path_to_file = load_path + file
     audio_file = AudioSegment.from_file(path_to_file , "wav")
     chunks = make_chunks(audio_file, chunk_length_ms)
@@ -15,6 +16,6 @@ for i, file in enumerate(files):
     #Export all of the individual chunks as wav files
 
     for i, chunk in enumerate(chunks):
-        chunk_name = "chunk{0}.wav".format(i)
+        chunk_name = save_path + "{0}-chunk{1}.wav".format(file_label, i)
         print "exporting", chunk_name
         chunk.export(chunk_name, format="wav")
